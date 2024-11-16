07639ab70c8c28876b05c0f38e795737bffc3de5
AldSenior <aldsio196@gmail.com>
2024-11-16 15:35:14 +0300

    v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Calculator.css'; // Стили остаются прежними
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Calculator = () => {
+    const [input, setInput] = useState('');
 
-export default App;
+    const buttons = [
+        '7', '8', '9', '/',
+        '4', '5', '6', '*',
+        '3', '2', '1', '-',
+        '0', '.', '=', '+'
+    ];
+
+    const isOperator = (char) => {
+        return ['+', '-', '*', '/', '='].includes(char);
+    };
+
+    const appendToDisplay = (value) => {
+        const lastChar = input[input.length - 1];
+
+
+        if (isOperator(value) && isOperator(lastChar)) {
+            return;
+        }
+
+        setInput((prevInput) => prevInput + value);
+    };
+
+    const clearDisplay = () => {
+        setInput('');
+    };
+
+    const calculateResult = () => {
+        try {
+            setInput(eval(input).toString());
+        } catch (error) {
+            alert('Ошибка в расчетах');
+            clearDisplay();
+        }
+    };
+
+    const handleButtonClick = (buttonValue) => {
+        if (buttonValue === '=') {
+            calculateResult();
+        }else {
+            appendToDisplay(buttonValue);
+        }
+    };
+
+    return (
+        <div className="calculator">
+            <div className="Pole">
+                <input type="text" value={input} disabled />
+            </div>
+
+            <div className="buttons">
+                {buttons.map((button, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleButtonClick(button)}
+                        className={isNaN(Number(button)) ? 'operation' : ''}
+                    >
+                        {button}
+                    </button>
+                ))}
+            </div>
+            <button onClick={clearDisplay} style={{
+                width: '100%',
+                marginBottom: '8px',
+            }}>C</button>
+        </div>
+    );
+};
+
+export default Calculator;
